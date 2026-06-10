f47e3f8b5f57a4304157539f7e2b9488715d080f
Sujit Pawar <sujitspawar9696@gmail.com>
2026-06-10 23:56:38 +0530

    Create Salary prediction.py

---
+++ Salary prediction.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+experience = np.array([1,2,3,4,5]).reshape(-1,1)
+salary = np.array([25000,30000,40000,50000,60000])
+
+model = LinearRegression()
+model.fit(experience, salary)
+
+exp = float(input("Years of Experience: "))
+print("Predicted Salary:", model.predict([[exp]])[0])
