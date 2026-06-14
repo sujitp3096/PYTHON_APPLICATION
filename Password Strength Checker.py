fa8f79a3cbfbc408b8f9dc532820edacab669307
Sujit Pawar <sujitspawar9696@gmail.com>
2026-06-14 23:34:51 +0530

    Create Password Strength Checker.py

---
+++ Password Strength Checker.py
@@ -0,0 +1,11 @@
+import re
+
+password = input("Enter Password: ")
+
+if (len(password) >= 8 and
+    re.search(r"[A-Z]", password) and
+    re.search(r"[a-z]", password) and
+    re.search(r"[0-9]", password)):
+    print("Strong Password")
+else:
+    print("Weak Password")
