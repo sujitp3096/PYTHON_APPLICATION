9a2786360c7ff90f3644e8b05cdd4e53fc3c9b35
Sujit Pawar <sujitspawar9696@gmail.com>
2026-06-15 22:25:48 +0530

    Update Resume Screening AI.py

--- Resume Screening AI.py
+++ Resume Screening AI.py
@@ -1,1 +1,7 @@
+skills = ["python", "machine learning", "sql"]
 
+resume = input("Paste Resume Text: ").lower()
+
+score = sum(1 for skill in skills if skill in resume)
+
+print("Match Score:", score, "/", len(skills))
