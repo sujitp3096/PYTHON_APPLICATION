1d6638e8f1acebed4215d3b81368aec6fc68c2e1
Sujit Pawar <sujitspawar9696@gmail.com>
2026-06-18 21:37:46 +0530

    Update Electricity Bill Calculator.py

--- Electricity Bill Calculator.py
+++ Electricity Bill Calculator.py
@@ -1,1 +1,10 @@
+units = int(input("Enter Units Consumed: "))
 
+if units <= 100:
+    bill = units * 5
+elif units <= 200:
+    bill = units * 7
+else:
+    bill = units * 10
+
+print("Electricity Bill:", bill)
