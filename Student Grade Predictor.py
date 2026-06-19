54225d4aaf1568ad86289c9b8688f938c3dd7f7b
Sujit Pawar <sujitspawar9696@gmail.com>
2026-06-19 22:13:47 +0530

    Update Student Grade Predictor.py

--- Student Grade Predictor.py
+++ Student Grade Predictor.py
@@ -1,1 +1,13 @@
 
+marks = float(input("Enter Marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Grade:", grade)
