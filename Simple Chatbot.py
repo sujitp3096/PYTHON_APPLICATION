0fe48ddd2198ba876bd52ee20ade42c2fe568b3c
Sujit Pawar <sujitspawar9696@gmail.com>
2026-06-21 22:53:12 +0530

    Update Simple Chatbot.py

--- Simple Chatbot.py
+++ Simple Chatbot.py
@@ -1,1 +1,12 @@
+while True:
+    msg = input("You: ").lower()
 
+    if msg == "hello":
+        print("Bot: Hi!")
+    elif msg == "how are you":
+        print("Bot: I am fine.")
+    elif msg == "bye":
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: I don't understand.")
