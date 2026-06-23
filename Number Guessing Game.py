1a8d2280c92fda5f6f23c63853cf054995aebbc4
Sujit Pawar <sujitspawar9696@gmail.com>
2026-06-23 23:08:39 +0530

    Update Number Guessing Game.py

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -1,1 +1,10 @@
+import random
 
+number = random.randint(1, 10)
+
+guess = int(input("Guess Number (1-10): "))
+
+if guess == number:
+    print("Correct!")
+else:
+    print("Wrong! Number was", number)
